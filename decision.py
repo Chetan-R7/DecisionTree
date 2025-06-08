a40242d67c5f282d871724fb203f59acd9268718
Chetan-R7 <chetan.7raj@gmail.com>
2025-06-08 16:54:29 +0530

    Added function to decision.py

--- decision.py
+++ decision.py
@@ -8,7 +8,6 @@ y=music_data["genre"]
 
 model=DecisionTreeClassifier()
 model.fit(X.values,y)
-
 #tree.export_graphviz(model,out_file='music-recommender.dot',feature_names=['age','gender'],class_names=sorted(y.unique()),label='all',rounded=True,filled=True)
 def predict(a,b):
     predictions=model.predict([[a,b]])
